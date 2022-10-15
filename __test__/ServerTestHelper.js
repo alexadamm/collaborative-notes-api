af778166409f27a919ad936c63ad770405d5b306
Alexander Adam <alexanderadamm21@gmail.com>
2022-10-15 21:08:07 +0700

    add server test helper for endpoint that require authentication

---
+++ __test__/ServerTestHelper.js
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+const ServerTestHelper = {
+  async newUser({ request, app }, {
+    username = 'johndoe', fullname = 'John Doe', password = 'secret',
+  }) {
+    const newUserResponse = await request(app).post('/users').send({ username, fullname, password });
+    const { id: userId } = newUserResponse.body.data;
+    const loginResponse = await request(app).post('/authentications').send({ username, password });
+    const { accessToken } = loginResponse.body.data;
+    return { userId, accessToken };
+  },
+};
+
+module.exports = ServerTestHelper;
